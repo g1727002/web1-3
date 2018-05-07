2427278a7c42431d599c295b3aa1e297bfe5211d
g1727002 <g1727002@st.t-kougei.ac.jp>
2018-05-07 18:11:16 +0900

    最初のコミット

--- Sample/Assets/Test.cs
+++ Sample/Assets/Test.cs
@@ -4,121 +4,116 @@ using UnityEngine;
 
 public class Test : MonoBehaviour {
 
-    // Use this for initialization
-    void Start()
+    void SayHello()
     {
-        Debug.Log("Hello,World");
+        Debug.Log("Hello");
+    }
 
-        int age;
-        age = 30;
-        Debug.Log(age);
-
-        float height1 = 165.0f;
-        float height2;
-        height2 = height1;
-        Debug.Log(height2);
-
-        string name;
-        name = "kitamura";
-        Debug.Log(name);
-
-        int answer = 10;
-        answer = 1 + 2;
-        Debug.Log(answer);
-
-        answer = 3 - 4;
-        Debug.Log(answer);
-
-        answer = 5 * 6;
-        Debug.Log(answer);
-
-        answer = 8 / 4;
-        Debug.Log(answer);
-
-        int n1 = 8;
-        int n2 = 9;
-        answer = n1 + n2;
-        Debug.Log(answer);
-
-        answer += 5;
-        Debug.Log(answer);
-
-        answer++;
-        Debug.Log(answer);
-
-        string str1 = "happy";
-        string str2 = "birthday";
-        string message;
-
-        message = str1 + str2;
-        Debug.Log(message);
-
-        string str = "happy";
-        int num = 123;
-        string text = str + num;
-        Debug.Log(text);
-
-        int herbNum = 1;
-        if (herbNum == 1)
-        {
-            Debug.Log("HPが50回復");
-        }
-
-        int hp = 200;
-        if (hp >= 100)
-        {
-            Debug.Log("攻撃！");
-        }
-        else
-        {
-            Debug.Log("防御！");
-        }
-
-        int hp2 = 180;
-        if (hp2 <= 50)
-        {
-            Debug.Log("逃走！");
-        }
-        else if (hp2 >= 200)
-        {
-            Debug.Log("攻撃！");
-        }
-        else
-        {
-            Debug.Log("防御！");
-        }
-
-        for(int i = 0; i < 5; i++)
-        {
-            Debug.Log(i);
-        }
-
-        for(int i = 0; i < 10; i += 2)
-        {
-            Debug.Log(i);
-        }
-
-        for(int i = 3; i <= 5; i++)
-        {
-            Debug.Log(i);
-        }
-
-        for(int i = 3; i >= 0; i--)
-        {
-            Debug.Log(i);
-        }
-
-        int sum = 0;
-        for(int i = 1; i <= 10; i++)
-        {
-            sum += i;
-        }
-        Debug.Log(sum);
-
+    void Callname(string name)
+    {
+        Debug.Log("Hello" + name);
     }
 
 
 
+    int Add(int a, int b) {
+        int c = a + b;
+        return c;
+    }
+    
+  
+
+
+
+}
+
+public class Player
+{
+
+    private int hp = 100;
+    private int power = 50;
+
+    public void Attack()
+    {
+        Debug.Log(this. power + "のダメージを受けた");
+
+    }
+    public void Damage(int damage)
+    {
+        this.hp -= damage;
+        Debug.Log(damage + "のダメージを受けた");
+
+    }
+}
+
+
+
+
+
+
+    // Use this for initialization
+    void Start() { 
+    {
+        int[] array = new int[5];
+
+        array[0] = 2;
+        array[1] = 10;
+        array[2] = 5;
+        array[3] = 15;
+        array[4] = 3;
+
+        for(int i = 0; i <5; i++)
+        {
+            Debug.Log(array[i]);
+        }
+
+        int[] points = { 83, 99, 52, 93, 15 };
+
+        for (int i = 0; i <= points.Length; i++) ;
+        if(points[i] >= 90)
+        {
+            Debug.Log(points[i]);
+        }
+
+
+        {
+            SayHello();
+        }
+
+
+        {
+            Callname("Tom");
+        }
+
+        int answer;
+        answer = Add(2, 3);
+        Debug.Log(answer);
+    }
+
+
+    {
+        Player myPlayer = new Player();
+        myPlayer.Attack();
+        myPlayer.Damage(30);
+    }
+
+    {
+        Vector2 playerPos = new Vector2(3.0f, 4.0f);
+        playerPos.x += 8.0f;
+        playerPos.y += 5.0f;
+        Debug.Log(playerPos);
+    }
+
+    {
+        Vector2 startPos = new Vector2(2.0f, 1.0f);
+        Vector2 endPos = new Vector2(8.0f, 5.0f);
+        Vector2 dir = endPos - startPos;
+        Debug.Log(dir);
+
+        float len = dir.magnitude;
+        Debug.Log(len);
+
+    }
 
 
     // Update is called once per frame
